7f360c72923304d4f2949a254e57b9e0e76e7e0b
Muhammad-Usman-Zafar <153068574+Muhammad-Usman-Zafar@users.noreply.github.com>
2023-12-06 17:44:50 +0500

    Add files via upload

--- Routers.js
+++ Routers.js
@@ -4,11 +4,12 @@ const Contact = require("../Schema/userSchema")
 
 const router = express.Router();
 
+
 router.route("/").get( expressAsyncHandler(async (req, res)=>{
     const user = await Contact.find();
     res.status(200).json(user);
 }));
-router.route("/:id").get( expressAsyncHandler(async (req, res)=>{
+router.route("/login/:id").get( expressAsyncHandler(async (req, res)=>{
     const userid = await Contact.findById(req.params.id);
     if(!userid){
         res.status(404);
@@ -17,12 +18,13 @@ router.route("/:id").get( expressAsyncHandler(async (req, res)=>{
     res.status(200).json(userid);
 }));
 
-router.route("/").post( expressAsyncHandler(async (req, res)=>{
-    const {name, email, role } = req.body;
+router.route("/create").post( expressAsyncHandler(async (req, res)=>{
+    const { name, email, role } = req.body;
     if (!name || !email || !role) {
         res.status(400);
         throw new Error("All Fields are mendatory!")
     }
+    
     const user = await Contact.create({
         name,
         email,
@@ -31,18 +33,41 @@ router.route("/").post( expressAsyncHandler(async (req, res)=>{
     res.status(200).json(user);
 }));
 
-router.route("/:id").put( expressAsyncHandler(async (req, res)=>{
+router.route("/update/:id").put( expressAsyncHandler(async (req, res) => {
     const userid = await Contact.findById(req.params.id);
-    if(!userid){
+    if (!userid) {
         res.status(404);
-        throw new Error("User Not Found!")
+        throw new Error("User Not Found!");
     }
+
+    if (req.body.role && req.body.role.toLowerCase() === "admin" && userid.role === "admin") {
+        res.status(403)
+        throw new Error("You can't update user")
+    }
+    if (req.body.role && req.body.role.toLowerCase() === "admin" && userid.role !== "admin") {
+        userid.role = "admin"
+        await userid.save();
+    }
+
     const updatedUser = await Contact.findByIdAndUpdate(
         req.params.id,
         req.body,
-        {new: true}
-    )
+        { new: true }
+    );
     res.status(200).json(updatedUser);
+    
+
+    
 }));
+
+router.route("/delete/:id").delete( expressAsyncHandler(async (req, res) => {
+    const user = await Contact.findByIdAndDelete(req.params.id);
+    if (!user) {
+        res.status(404).json({ error: "User not found" });
+        return;
+    }
+    res.status(200).json({ message: "User deleted successfully" });
+}));
+
 
 module.exports = router;
